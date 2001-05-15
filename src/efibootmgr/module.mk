c138b6297915c2c571932ac029f7b0892926074a
mebrown <mebrown>
2001-05-15 19:52:36 +0000

    readability tweaks to makefiles

--- src/efibootmgr/module.mk
+++ src/efibootmgr/module.mk
@@ -1,17 +1,21 @@
 efibootmgr_SRCDIR := src/efibootmgr
 efibootmgr_OBJECTS := efibootmgr.o
 efibootmgr_TARGETS := efibootmgr
+efibootmgr_FULLTARGET :=  \
+  	$(patsubst %, $(efibootmgr_SRCDIR)/%, $(efibootmgr_TARGETS))
+efibootmgr_FULLOBJECT :=  \
+  	$(patsubst %, $(efibootmgr_SRCDIR)/%, $(efibootmgr_OBJECT))
 
 efibootmgr_LIBS    := crc32.o disk.o efi.o efichar.o gpt.o scsi_ioctls.o \
                       unparse_path.o
 efibootmgr_LIBDIR  := src/lib
-
-ALLDEPS += $(patsubst %, $(efibootmgr_SRCDIR)/%, $(efibootmgr_TARGETS))
-CLEANLIST += $(patsubst %, $(efibootmgr_SRCDIR)/%, $(efibootmgr_TARGETS))
-CLEANLIST += $(patsubst %, $(efibootmgr_SRCDIR)/%, $(efibootmgr_OBJECTS))
-
-CFLAGS += -Isrc/include -Isrc/lib
-
-$(patsubst %, $(efibootmgr_SRCDIR)/%, $(efibootmgr_TARGETS)):  \
-  	$(patsubst %,$(efibootmgr_SRCDIR)/%,$(efibootmgr_OBJECTS)) \
+efibootmgr_FULLLIB := \
   	$(patsubst %,$(efibootmgr_LIBDIR)/%,$(efibootmgr_LIBS))
+
+ALLDEPS += $(efibootmgr_FULLTARGET) 
+CLEANLIST += $(efibootmgr_FULLTARGET)
+CLEANLIST += $(efibootmgr_FULLOBJECT)
+
+$(efibootmgr_FULLTARGET): \
+  	$(efibootmgr_FULLOBJECT) \
+  	$(efibootmgr_FULLLIB)
