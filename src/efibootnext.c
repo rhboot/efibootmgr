325d7d94a873fac2e64aeb0312ae09a23bf9874a
Robert Bisewski <contact@ibiscybernetics.com>
2017-12-26 17:35:41 -0600

    Update docs for old URLs and general formatting.

--- src/efibootnext.c
+++ src/efibootnext.c
@@ -16,7 +16,7 @@
  * for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this library; if not, see <http://www.gnu.org/licenses/>.
+ * with this library; if not, see <https://www.gnu.org/licenses/>.
  *
  */
 
